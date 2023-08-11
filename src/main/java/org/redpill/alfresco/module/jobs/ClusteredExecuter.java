8ebde21a46b1aacc42cc18bbf4085b3a5cb44698
Andreas Lindström-Åberg <andreas.lindstrom-aberg@redpill-linpro.com>
2023-08-11 15:22:52 +0200

    Revert "Update to use SDK 4.6.0 (7.4)"
    This reverts commit 692cbd8ba615f01345a1498708616d54d4dc2b7c.

--- src/main/java/org/redpill/alfresco/module/jobs/ClusteredExecuter.java
+++ src/main/java/org/redpill/alfresco/module/jobs/ClusteredExecuter.java
@@ -10,8 +10,7 @@ import org.alfresco.service.transaction.TransactionService;
 import org.alfresco.util.PropertyCheck;
 import org.alfresco.util.VmShutdownListener;
 import org.apache.commons.lang.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.log4j.Logger;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
@@ -25,7 +24,7 @@ import org.springframework.beans.factory.InitializingBean;
  */
 public abstract class ClusteredExecuter implements InitializingBean, Job {
 
-  private static final Logger LOG = LoggerFactory.getLogger(ClusteredExecuter.class);
+  private static final Logger LOG = Logger.getLogger(ClusteredExecuter.class);
 
   //Default lock ttl 5 min 30 sec
   public static final long DEFAULT_LOCK_TTL = 330000L;
